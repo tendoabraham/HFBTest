eb95604df122f1dcbcc5df9b18a5f4d1c74adc73
Tendo Abraham <tendoabraham@gmail.com>
2023-05-30 12:42:33 +0300

    Data and OTP

---
+++ app/src/main/java/com/elmahousingfinanceug_test/main_Pages/Airtime_And_Data_Options.java
@@ -0,0 +1,14 @@
+package com.elmahousingfinanceug_test.main_Pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Airtime_And_Data_Options extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_airtime_and_data_options);
+    }
+}
---
+++ app/src/main/java/com/elmahousingfinanceug_test/main_Pages/Data.java
@@ -0,0 +1,14 @@
+package com.elmahousingfinanceug_test.main_Pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+    }
+}
